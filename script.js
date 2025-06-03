30f9b7fac789ecee372d0a725155ac46c1806d28
Minhaz Uddin <minhazrock1@gmail.com>
2025-06-03 15:18:44 +0530

    update

--- script.js
+++ script.js
@@ -105,7 +105,9 @@ async function copyContent() {
 function shufflePassword(array) {
     // fisher yates method
     for (let i = array.length - 1; i > 0; i--) {
+        //  random J, find out using random function
         const j = Math.floor(Math.random() * (i + 1));
+        // swap number at i index and j index
         const temp = array[i];
         array[i] = array[j];
         array[j] = temp;
